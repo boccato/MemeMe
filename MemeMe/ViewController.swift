9c0e2aa67c57c56d879eda22f453545a4ada337a
Ricardo Boccato Alves <boccato@gmail.com>
2015-09-20 13:31:20 -0300

    Constraints for UIToolbar, UIImageView and UITextFields.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var btnCamera: UIBarButtonItem!
+    @IBOutlet weak var edtBottom: UITextField!
+    @IBOutlet weak var edtTop: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        btnCamera.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName: UIColor.blackColor(),
+            NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName: 2
+        ]
+        edtBottom.defaultTextAttributes = memeTextAttributes
+        edtTop.defaultTextAttributes = memeTextAttributes
+        
+        edtBottom.textAlignment = .Center
+        edtTop.textAlignment = .Center
+        
+        edtBottom.text = "BOTTOM"
+        edtTop.text = "TOP"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickAnImageFromAlbum(sender: UIBarButtonItem) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .SavedPhotosAlbum
+        self.presentViewController(picker, animated: true, completion: nil)
     }
-
+    
+    @IBAction func pickAnImageFromCamera(sender: UIBarButtonItem) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .Camera
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.imagePickerView.image = image
+        }
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
 
 }
-
