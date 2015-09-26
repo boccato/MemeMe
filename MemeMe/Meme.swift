6bb9a21ecf37087bf506cf0dee99a2e8c8acb0c8
Ricardo Boccato Alves <boccato@gmail.com>
2015-09-26 16:34:55 -0300

    ADDED: view moves with keyboard ADDED: better button icons

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Ricardo Boccato Alves on 9/26/15.
+//  Copyright © 2015 Ricardo Boccato Alves. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var bottom: String;
+    var top: String;
+    var image: UIImage
+    var memedImage: UIImage
+}
--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,21 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var btnCamera: UIBarButtonItem!
     @IBOutlet weak var edtBottom: UITextField!
     @IBOutlet weak var edtTop: UITextField!
+    
+    var keyboardUp: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        edtBottom.delegate = self
+        edtTop.delegate = self
         btnCamera.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
         let memeTextAttributes = [
             NSStrokeColorAttributeName: UIColor.blackColor(),
             NSForegroundColorAttributeName: UIColor.whiteColor(),
             NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-            NSStrokeWidthAttributeName: 2
+            NSStrokeWidthAttributeName: -1
         ]
         edtBottom.defaultTextAttributes = memeTextAttributes
         edtTop.defaultTextAttributes = memeTextAttributes
@@ -33,11 +37,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         edtBottom.text = "BOTTOM"
         edtTop.text = "TOP"
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unsubscribeToKeyboardNotifications()
+    }
 
     @IBAction func pickAnImageFromAlbum(sender: UIBarButtonItem) {
         let picker = UIImagePickerController()
         picker.delegate = self
-        picker.sourceType = .SavedPhotosAlbum
+        picker.sourceType = .PhotoLibrary
         self.presentViewController(picker, animated: true, completion: nil)
     }
     
@@ -59,4 +73,56 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         self.dismissViewControllerAnimated(true, completion: nil)
     }
 
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Move the view when the keyboard covers the text field.
+    func keyboardWillShow(notification: NSNotification) {
+        if edtBottom.isFirstResponder() && !self.keyboardUp {
+            self.view.frame.origin.y -= getKeyboardHeight(notification)
+            self.keyboardUp = true
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if self.keyboardUp {
+            self.keyboardUp = false
+            self.view.frame.origin.y += getKeyboardHeight(notification)
+        }
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "keyboardWillShow:", object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "keyboardWillHide:", object: nil)
+    }
+    
+    func generateMemedImage() -> UIImage {
+        
+        // TODO: Hide toolbar and navbar
+        
+        // Render view to an image
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        self.view.drawViewHierarchyInRect(self.view.frame,
+            afterScreenUpdates: true)
+        let memedImage : UIImage =
+        UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        // TODO:  Show toolbar and navbar
+        
+        return memedImage
+    }
 }
